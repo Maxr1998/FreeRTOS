ec4ec74d1173175e73afac24707932a5734a4587
Maxr1998 <max.rumpf1998@gmail.com>
2023-11-18 22:53:32 +0100

    Configure for Linux

--- FreeRTOS/Demo/CORTEX_M4_ATSAM4S_Atmel_Studio/src/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_M4_ATSAM4S_Atmel_Studio/src/FreeRTOSConfig.h
@@ -28,13 +28,6 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/*
- * The following #error directive is to remind users that a batch file must be
- * executed prior to this project being built.  Once it has been executed
- * remove the #error line below.
- */
-#error Ensure CreateProjectDirectoryStructure.bat has been executed before building.  See comment immediately above.
-
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
